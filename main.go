3407bb23f3c33e1dd5e930afb88d0cd9b7380cc9
Nattawut J <nattawut.j@th.club21global.com>
2025-07-27 00:27:47 +0700

    commit 2

--- main.go
+++ main.go
@@ -5,4 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("================ hello ========= 33333")
 	fmt.Println("This is a test repository for Go code.")
+	fmt.Println("You can add your Go code here and run it.")
 }
