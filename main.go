2589a707f5fe06548f2c97924bf7740153a2997e
IT-Ha <hag2539@gmail.com>
2025-09-29 00:26:21 +0700

    5555

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package testrepo
 import "fmt"
 
 func main() {
-	fmt.Println("================ hello ========= 4444")
+	fmt.Println("================ hello ========= 5555")
 	fmt.Println("This is a test repository for Go code.")
 	fmt.Println("You can add your Go code here and run it.")
 	fmt.Println("Enjoy coding in Go!")
