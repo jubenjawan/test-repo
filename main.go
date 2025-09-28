59938d5aa187cc3672f7459b25859a481a72b93a
IT-Ha <hag2539@gmail.com>
2025-09-29 00:07:44 +0700

    commit 3

--- main.go
+++ main.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Println("================ hello ========= 33333")
 	fmt.Println("This is a test repository for Go code.")
 	fmt.Println("You can add your Go code here and run it.")
+	fmt.Println("Enjoy coding in Go!")
 }
